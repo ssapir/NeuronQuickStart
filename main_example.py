304a3a6395a5312dd8157bafbfcef93b350f415a
sapir.shapira <sapir.shapira@cs.huji.ac.il>
2023-11-01 13:41:38 +0200

    Main refactor for readability

--- main_example.py
+++ main_example.py
@@ -51,34 +51,8 @@ def compare_swc_without_header(swc_str1: str, swc_str2: str, ignore_ids=True):
                 # print(ind, l1)
 
 
-def load():  # todo currently fails because the morph has no types (must have soma, apical & basal. it's undefined for h01 swc files)
-    cell1 = NeuronCell(use_cvode=True, morphologyFilename=os.path.join("morphologies", "4559469707.swc"),
-                       biophysicalModelTemplateFilename="generic_template.hoc", templateName="Celltemplate",
-                       biophysicalModelFilename=None,  # inside template already
-                       model_path=os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])),
-                                               "h01_Human_swc_examples"))
-    print(cell1.soma, cell1.apical_sections)
-    fig, ax = plt.subplots(1, 1, figsize=(8, 8))
-    plot_morphology_from_cell(ax, cell1, color_by_type=True, fontsize=8)
-    ax.axis('equal')
-    plt.tight_layout()
-    plt.show()
-
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-
-    # swc converters
-    if False:
-        with open(os.path.join("h01_Human_swc_examples", "morphologies", "4559469707.swc"), "r") as f:
-            swc_content = f.read()
-            skeleton: Skeleton = FactoryNeuronCell.swc_to_skeleton(swc_content=swc_content)
-            print(skeleton)
-            print(skeleton.vertex_types)
-            swc_str: str = FactoryNeuronCell.skeleton_to_swc(skeleton=skeleton)
-            compare_swc_without_header(swc_str1=swc_content, swc_str2=swc_str)
-            # with open(os.path.join("h01_Human_swc_examples", "morphologies", "4559469707_rewritten.swc"), "w") as f:
-            #     f.write(swc_str)
 
     # load neuron as cell (neuron h wrapper)
     use_neurom = True  # if installed (pip install neurom)
@@ -97,3 +71,28 @@ if __name__ == '__main__':
 
     if use_neurom:
         use_neurom_visualization(curr_path)
+
+    # swc converters - will work with proper types / after fixing soma/apical/basal in downloaded swc
+    if False:
+        with open(os.path.join("h01_Human_swc_examples", "morphologies", "4559469707.swc"), "r") as f:
+            swc_content = f.read()
+            skeleton: Skeleton = FactoryNeuronCell.swc_to_skeleton(swc_content=swc_content)
+            print(skeleton)
+            print(skeleton.vertex_types)
+            swc_str: str = FactoryNeuronCell.skeleton_to_swc(skeleton=skeleton)
+            compare_swc_without_header(swc_str1=swc_content, swc_str2=swc_str)
+            with open(os.path.join("h01_Human_swc_examples", "morphologies", "4559469707_rewritten.swc"), "w") as f:
+                f.write(swc_str)
+
+        # todo currently fails because the morph has no types (must have soma, apical & basal. it's undefined for h01 swc files)
+        cell1 = NeuronCell(use_cvode=True, morphologyFilename=os.path.join("morphologies", "4559469707.swc"),
+                           biophysicalModelTemplateFilename="generic_template.hoc", templateName="Celltemplate",
+                           biophysicalModelFilename=None,  # inside template already
+                           model_path=os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])),
+                                                   "h01_Human_swc_examples"))
+        print(cell1.soma, cell1.apical_sections)
+        fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+        plot_morphology_from_cell(ax, cell1, color_by_type=True, fontsize=8)
+        ax.axis('equal')
+        plt.tight_layout()
+        plt.show()
